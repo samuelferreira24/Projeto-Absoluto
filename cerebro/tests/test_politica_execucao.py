cf45c10435520125fe29a06d974bd94640be25ea
samuelferreira24 <samuelferreirajunior24@gmail.com>
2026-09-17 13:46:29 -0300

    test(cerebro): validar limites de autonomia da execução

---
+++ cerebro/tests/test_politica_execucao.py
@@ -0,0 +1,17 @@
+from cerebro.politica_execucao import Acao, NivelAutonomia, PoliticaExecucao
+
+
+def test_politica_permite_acoes_reversiveis_no_limite():
+    politica = PoliticaExecucao(NivelAutonomia.ACOES_REVERSIVEIS)
+    assert politica.autorizada(Acao("pesquisar", NivelAutonomia.PESQUISAR))
+    assert politica.autorizada(Acao("criar_branch", NivelAutonomia.DELEGAR))
+
+
+def test_politica_bloqueia_alem_do_limite():
+    politica = PoliticaExecucao(NivelAutonomia.PESQUISAR)
+    assert not politica.autorizada(Acao("criar_branch", NivelAutonomia.DELEGAR))
+
+
+def test_autorizacao_explicita_nao_e_concedida_pela_politica():
+    politica = PoliticaExecucao(NivelAutonomia.ALTERAR_SISTEMAS_AUTORIZADOS)
+    assert not politica.autorizada(Acao("acao_sensivel", NivelAutonomia.ALTERAR_SISTEMAS_AUTORIZADOS, exige_autorizacao=True))
