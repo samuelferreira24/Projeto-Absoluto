d863716a6c361e20561fe152390b98e27a6fcca3
samuelferreira24 <samuelferreirajunior24@gmail.com>
2026-09-17 23:22:16 -0300

    test(seguranca): validar aprovacao vinculada

--- cerebro/tests/test_politica_execucao.py
+++ cerebro/tests/test_politica_execucao.py
@@ -15,3 +15,18 @@ def test_politica_bloqueia_alem_do_limite():
 def test_autorizacao_explicita_nao_e_concedida_pela_politica():
     politica = PoliticaExecucao(NivelAutonomia.ALTERAR_SISTEMAS_AUTORIZADOS)
     assert not politica.autorizada(Acao("acao_sensivel", NivelAutonomia.ALTERAR_SISTEMAS_AUTORIZADOS, exige_autorizacao=True))
+
+
+def test_aprovacao_vinculada_e_consumida():
+    acao = Acao("acao_sensivel", NivelAutonomia.ALTERAR_SISTEMAS_AUTORIZADOS, exige_autorizacao=True)
+    politica = PoliticaExecucao(NivelAutonomia.ALTERAR_SISTEMAS_AUTORIZADOS)
+    token = politica.emitir_aprovacao(acao, ferramenta="git", recurso="repo-x", custo_maximo=2.0)
+    assert politica.autorizada(acao, ferramenta="git", recurso="repo-x", custo_estimado=1.0, aprovacao_id=token)
+    politica.consumir_aprovacao(token)
+    assert not politica.autorizada(acao, ferramenta="git", recurso="repo-x", custo_estimado=1.0, aprovacao_id=token)
+
+def test_aprovacao_nao_pode_ser_reutilizada_para_outra_ferramenta():
+    acao = Acao("acao_sensivel", NivelAutonomia.ALTERAR_SISTEMAS_AUTORIZADOS, exige_autorizacao=True)
+    politica = PoliticaExecucao(NivelAutonomia.ALTERAR_SISTEMAS_AUTORIZADOS)
+    token = politica.emitir_aprovacao(acao, ferramenta="git", recurso="repo-x")
+    assert not politica.autorizada(acao, ferramenta="shell", recurso="repo-x", aprovacao_id=token)
