104d23bbd4da4e78ea4b5889fe58857b18326451
samuelferreira24 <samuelferreirajunior24@gmail.com>
2026-09-23 16:27:10 -0300

    Add Project Knowledge sync entrypoint

---
+++ scripts/project_knowledge_sync.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""Synchronize observable Projeto Absoluto knowledge and map projections."""
+from abs_core.project_knowledge import main
+
+if __name__ == "__main__":
+    raise SystemExit(main())
